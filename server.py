f486ca7a1b03f0cb52db974fe9dddc3267974da8
Brian Tsau <bt2420@columbia.edu>
2018-04-11 11:19:20 -0400

    fix to session

--- server.py
+++ server.py
@@ -80,7 +80,7 @@ def cpu_index():
     all_cpus = []
     all_ids = []
     query = '''SELECT * FROM cpu c, cpu_sockets cs WHERE cs.mobo_id = {} AND c.cpu_id =
- cs.cpu_id'''.format(session['mobo_id']) if session['socket'] else "SELECT * FROM cpu"
+ cs.cpu_id'''.format(session['mobo_id']) if 'socket' in session else "SELECT * FROM cpu"
     print >> sys.stderr, query
 
     cursor = g.conn.execute(query)
@@ -108,7 +108,7 @@ def motherboard_index():
     all_ids = []
 
     form_conditional = ''
-    if session['form_factor']:
+    if 'cpu_id' in session:
         if 'case_id' in session:
             form_conditional = ' AND ff.case_id = {}'.format(session['case_id'])
         if 'psu_id' in session:
@@ -116,7 +116,7 @@ def motherboard_index():
     else:
         query = '''SELECT * FROM motherboard m, cpu_sockets cs, form factor ff WHERE cs.cpu_id = {}
  AND m.mobo_id = cs.mobo_id'''.format(
-            session['mobo_id']) if session['socket'] else "SELECT * FROM motherboard"
+            session['cpu_id']) if session['socket'] else "SELECT * FROM motherboard"
 
     query = "{}{}".format(query, form_conditional)
     print >> sys.stderr, query
